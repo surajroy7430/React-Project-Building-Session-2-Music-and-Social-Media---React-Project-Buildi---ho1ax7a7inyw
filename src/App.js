4345d5b9b91de4dfca15ddfe3d4fc92085c53bf6
Suraj Roy <surajroy7430@gmail.com>
2023-11-24 21:16:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header/Header';
+import Main from './Components/Main/Main';
+import MusicPlayer from './Components/MusicPlayer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <MusicPlayer />
+    </div>
+  );
+}
+
+export default App;
