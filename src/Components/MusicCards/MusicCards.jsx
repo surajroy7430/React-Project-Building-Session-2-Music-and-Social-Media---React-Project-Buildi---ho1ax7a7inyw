a8f5dd9ea17e5e1e904a36be3ed5721650bfea75
Suraj Roy <surajroy7430@gmail.com>
2024-01-10 14:52:58 +0530

    audioPlayer complete

--- src/Components/MusicCards/MusicCards.jsx
+++ src/Components/MusicCards/MusicCards.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MusicCards.css';
+import { useMusic } from '../../utils/MusicProvider';
+import { Add, Menu, PlayCircle } from '@mui/icons-material';
 
 const MusicCards = (props) => {
+  const { selectedMusic, selectMusic } = useMusic();
   const { _id, artist, audio_url, title, thumbnail } = props;
   const artistsList = artist.map((item) => item.name).join(", ");
+
+  const isSelected = selectedMusic && selectedMusic._id === _id;
+
   return (
     <section className='musicCard'>
-      <img 
-        src={thumbnail} 
-        alt={title}
-        className='thumbnailImg'
-      />
-      <div className='songTitle' title={title}>
+      <div
+        className='thumbnailContainer' 
+        style={{ 
+          backgroundImage: `url(${thumbnail})`, 
+          backgroundRepeat: 'no-repeat', 
+          backgroundSize: 'cover'
+        }}
+        onClick={() => selectMusic(props)}
+      >
+        
+        {isSelected ? (
+          <div className='extraIcons'>
+            <img 
+              src='https://m.media-amazon.com/images/G/01/digital/music/player/web/EQ_accent.webp' 
+              className='playingIcon'
+            />
+          </div>
+        ) : null}
+        
+      </div>
+
+      <div className='songTitle' title={title} onClick={() => selectMusic(props)}>
         {title}
       </div>
       <div className='songArtists' title={artistsList}>
--- src/Components/Musics/MusicPlayer.jsx
+++ src/Components/Musics/MusicPlayer.jsx
@@ -1,17 +1,70 @@
-import { AppBar, Toolbar } from '@mui/material'
-import React from 'react'
+import { Avatar, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useMusic } from '../../utils/MusicProvider';
+import H5AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
-const MusicPlayer = () => {
+const MusicPlayer = ({ songs }) => {
+  const { selectedMusic, selectMusic } = useMusic();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const findIndexById = (id) => {
+    return songs.findIndex((song) => song._id === id);
+  };
+  useEffect(() => {
+    // Reset the current index when a new music is selected
+    setCurrentIndex(findIndexById(selectedMusic._id));
+  }, [selectedMusic]);
+
+  const { artist, audio_url, title, thumbnail } = selectedMusic;
+  const artists = artist && artist.map((item) => item.name).join(", ");
+
+  const changeSong = (direction) => {
+    const totalSongs = songs.length;
+    const newIndex =
+      direction === 'next'
+        ? (currentIndex + 1) % totalSongs
+        : (currentIndex - 1 + totalSongs) % totalSongs;
+
+    setCurrentIndex(newIndex);
+    selectMusic(songs[newIndex]);
+  };
+
+  const CustomControls = () => (
+    <div className='musicDetails'>
+      <div style={{display: 'flex'}}>
+        <Avatar 
+          sx={{ width: 56, height: 56 }} 
+          variant="square" 
+          src={thumbnail}
+        />
+        <div style={{marginLeft: '15px'}}>
+          <Typography className="musicPlayerTitle">
+            {title}
+          </Typography>
+          <Typography className="musicPlayerArtist">
+            {artists}
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+
+  if (!title) {
+    return <></>; // Render nothing if no music is selected
+  }
+
   return (
-    <AppBar 
-      position="fixed" 
-      elevation={0}
-      sx={{ top: 'auto', bottom: 0 }} 
-      className='musicPlayer'
-    >
-      <div className="musicTimer"></div>
-      <div className='musicDetails'></div>
-    </AppBar>
+    <H5AudioPlayer 
+      autoPlay={true}
+      autoPlayAfterSrcChange={true}
+      src={audio_url}
+      volume={0.2}
+      customAdditionalControls={[<CustomControls />]}
+      showSkipControls={true}
+      onClickPrevious={() => changeSong('previous')}
+      onClickNext={() => changeSong('next')}
+    />
   )
 }
 
